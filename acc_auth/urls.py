abf87aa0790c2f14c4cff779b2b86a82d840c745
ekroma <magma0922@gmail.com>
2023-02-14 21:47:18 +0600

    added module2

---
+++ acc_auth/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import Qwiz, QuestionDetail
+from .views import (
+    RegistrationView, 
+    AccountActivationView,
+    ChangePasswordView,
+    RestorePasswordView,
+    SetRestoredPasswordView,
+    DeleteAccountView
+    )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+router = DefaultRouter()
+router.register(r'register/questions', Qwiz,'register/questions')
+
+
+urlpatterns = [
+    path('register/', RegistrationView.as_view(), name='registration'),
+    path('activate/<str:activation_code>/', AccountActivationView.as_view(), name='activation'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('change-password/', ChangePasswordView.as_view(), name='change_password'),
+    path('restore-password/', RestorePasswordView.as_view(), name='restore_password'),
+    path('set-restored-password/', SetRestoredPasswordView.as_view(), name='set_restored_password'),
+    path('delete-account/', DeleteAccountView.as_view(), name='delete_account')
+] + router.urls
